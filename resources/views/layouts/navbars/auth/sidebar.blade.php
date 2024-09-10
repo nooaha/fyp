02134873293c4e3dd4847c7c7c7c89a77e7f5d9e
nooahaa <nuhazakiudin@gmail.com>
2024-09-10 19:28:48 +0800

    Edit menu milestone

--- resources/views/layouts/navbars/auth/sidebar.blade.php
+++ resources/views/layouts/navbars/auth/sidebar.blade.php
@@ -103,7 +103,7 @@
       </li>
       <!--Nav Milestone-->
       <li class="nav-item">
-        <a class="nav-link {{ (Request::is('billing') ? 'active' : '') }}" href="{{ url('billing') }}">
+        <a class="nav-link {{ (Request::is('pencapaian-perkembangan') ? 'active' : '') }}" href="{{ url('pencapaian-perkembangan') }}">
           <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
             <svg width="12px" height="12px" viewBox="0 0 43 36" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
               <title>credit-card</title>
