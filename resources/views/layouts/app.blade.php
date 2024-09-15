184888ebe8718a70a7d57fc1379476dad78634e0
nooahaa <nuhazakiudin@gmail.com>
2024-09-16 00:15:18 +0800

    Modify page title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,8 +31,8 @@
 
   <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
   <link rel="icon" type="image/png" href="../assets/img/favicon.png">
-  <title>
-    PediPulse
+  <title class="breadcrumb-item text-sm text-dark active capitalize-text" aria-current="page">PediPulse - 
+  {{ str_replace('-', ' ', Request::path()) }}
   </title>
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
--- resources/views/layouts/navbars/auth/nav.blade.php
+++ resources/views/layouts/navbars/auth/nav.blade.php
@@ -5,7 +5,9 @@
             <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
             <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Pages</a></li>
             <!--Page name atas-->
-            <li class="breadcrumb-item text-sm text-dark active text-capitalize" aria-current="page">{{ str_replace('-', ' ', Request::path()) }} (page name)</li>
+            <li class="breadcrumb-item text-sm text-dark active text-capitalize" aria-current="page">
+            {{ str_replace('-', ' ', Request::path()) }} (page name)
+        </li>
             </ol>
             <!--Page Header-->
             <h6 class="font-weight-bolder mb-0 text-capitalize">{{ str_replace('-', ' ', Request::path()) }} (page header)</h6>
