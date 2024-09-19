aa3243d3a710b3de0e2576e65d678bf75fd0d1d0
nooahaa <nuhazakiudin@gmail.com>
2024-09-19 20:39:26 +0800

    Add new file for navbar + modified the old one

---
+++ resources/views/layouts/navbars/guest/nav-welcome.blade.php
@@ -0,0 +1,65 @@
+<!-- Navbar -->
+<nav
+  class="navbar navbar-expand-lg position-absolute top-0 z-index-3 my-3 {{ (Request::is('static-sign-up') ? 'w-100 shadow-none  navbar-transparent mt-4' : 'blur blur-rounded shadow py-2 start-0 end-0 mx4') }}">
+  <div class="container-fluid {{ (Request::is('static-sign-up') ? 'container' : 'container-fluid') }}">
+    <!--<a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 >
+      <h5>PediPulse</h5>
+    </a>-->
+    <a class="align-items-center d-flex justify-content-center m-0 navbar-brand text-wrap" {{ (Request::is('static-sign-up') ? 'text-white' : '') }}" href="{{ url('/') }}">
+      <img src="{{ asset('assets/img/logo.png') }}" alt="PediPulse Logo" style="max-height: 60px; width: auto;">
+    </a>
+
+    <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation"
+      aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon mt-2">
+        <span class="navbar-toggler-bar bar1"></span>
+        <span class="navbar-toggler-bar bar2"></span>
+        <span class="navbar-toggler-bar bar3"></span>
+      </span>
+    </button>
+    <div class="collapse navbar-collapse" id="navigation">
+      <ul class="navbar-nav mx-auto">
+        @if (auth()->user())
+      <li class="nav-item">
+        <a class="nav-link d-flex align-items-center me-2 active" aria-current="page" href="{{ url('dashboard') }}">
+        <i class="fa fa-chart-pie opacity-6 me-1 {{ (Request::is('static-sign-up') ? '' : 'text-dark') }}"></i>
+        Dashboard
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link me-2" href="{{ url('profile') }}">
+        <i class="fa fa-user opacity-6 me-1 {{ (Request::is('static-sign-up') ? '' : 'text-dark') }}"></i>
+        Profile
+        </a>
+      </li>
+    @endif
+        <li class="nav-item">
+          <a class="nav-link me-2" href="#home">
+            Halaman Utama
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link me-2" href="#aboutUs">
+            Tentang Kami
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link me-2" href="#contactUs">
+            Hubungi Kami
+          </a>
+        </li>
+      </ul>
+      <ul class="navbar-nav d-flex justify-content-end pt-2">
+        <li class="nav-item">
+          <button type="button" class="btn btn-primary me-2"
+            href="">Log Masuk</button>
+        </li>
+        <li class="nav-item">
+          <button type="button" class="btn btn-secondary"
+            href="">Daftar</button>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<!-- End Navbar -->
--- resources/views/layouts/user_type/guest.blade.php
+++ resources/views/layouts/user_type/guest.blade.php
@@ -4,6 +4,16 @@
     @if(\Request::is('login/forgot-password')) 
         @include('layouts.navbars.guest.nav')
         @yield('content') 
+    @elseif(\Request::is('/'))
+        <div class="container position-sticky z-index-sticky top-0">
+            <div class="row">
+                <div class="col-12">
+                @include('layouts.navbars.guest.nav-welcome')
+                </div>
+            </div>
+        </div>
+        @yield('content')
+        @include('layouts.footers.guest.footer')
     @else
         <div class="container position-sticky z-index-sticky top-0">
             <div class="row">
