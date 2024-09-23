1098cd4c8d8e26ace367b50caecdd75f7bcb7a16
nooahaa <nuhazakiudin@gmail.com>
2024-09-24 01:22:05 +0800

    Modify all files to connect with db

--- resources/views/admin/admin-dashboard.blade.php
+++ resources/views/admin/admin-dashboard.blade.php
@@ -3,168 +3,182 @@
 @section('content')
 
 <div class="container">
-    <h3 class="mb-3">Selamat Kembali, </h3>
+    <h3 class="mb-3">Selamat Kembali, <span style="color: #3F51B2;">{{ Auth::user()->name }}!</span></h3>
     <h6 class="ms-2 mb-3 text-uppercase text-xs font-weight-bolder opacity-6">Paparan Data Pengguna</h6>
     <div class="row mb-4">
         <!--card kat dashboard 1st line-->
         <div class="col-md-3 mb-4">
-        <div class="card">
-            <div class="card-body p-3">
-            <div class="row">
-                <div class="col-8">
-                <div class="numbers">
-                    <p class="text-sm mb-0 text-capitalize font-weight-bold">Pengguna Berdaftar</p>
-                    <h5 class="font-weight-bolder mb-0">
-                    $53,000
-                    <span class="text-success text-sm font-weight-bolder">+55%</span>
-                    </h5>
-                </div>
-                </div>
-                <div class="col-4 text-end">
-                <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
-                    <i class="ni ni-money-coins text-lg opacity-10" aria-hidden="true"></i>
-                </div>
-                </div>
-            </div>
-            </div>
-        </div>
-        </div>
-        <div class="col-md-3 mb-4">
-            <div class="card">
+            <div class="card h-100">
                 <div class="card-body p-3">
-                <div class="row">
-                    <div class="col-8">
-                    <div class="numbers">
-                        <p class="text-sm mb-0 text-capitalize font-weight-bold">Pengguna Aktif</p>
-                        <h5 class="font-weight-bolder mb-0">
-                        2,300
-                        <span class="text-success text-sm font-weight-bolder">+3%</span>
-                        </h5>
+                    <div class="row">
+                        <div class="col-8">
+                            <div class="numbers">
+                                <p class="text-sm mb-0 text-capitalize font-weight-bold">Pengguna Berdaftar</p>
+                                <h5 class="font-weight-bolder mb-0">
+                                    $53,000
+                                    <span class="text-success text-sm font-weight-bolder">+55%</span>
+                                </h5>
+                            </div>
+                        </div>
+                        <div class="col-4 text-end">
+                            <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                                <i style="font-size: 1rem;" class="fas fa-lg fa-users ps-2 pe-2 text-center text-dark "
+                                    aria-hidden="true"></i>
+                            </div>
+                        </div>
                     </div>
-                    </div>
-                    <div class="col-4 text-end">
-                    <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
-                        <i class="ni ni-world text-lg opacity-10" aria-hidden="true"></i>
-                    </div>
-                    </div>
-                </div>
                 </div>
             </div>
         </div>
         <div class="col-md-3 mb-4">
-        <div class="card">
-            <div class="card-body p-3">
-            <div class="row">
-                <div class="col-8">
-                <div class="numbers">
-                    <p class="text-sm mb-0 text-capitalize font-weight-bold">Nyahaktif Pengguna</p>
-                    <h5 class="font-weight-bolder mb-0">
-                    +3,462
-                    <span class="text-danger text-sm font-weight-bolder">-2%</span>
-                    </h5>
-                </div>
-                </div>
-                <div class="col-4 text-end">
-                <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
-                    <i class="ni ni-paper-diploma text-lg opacity-10" aria-hidden="true"></i>
-                </div>
+            <div class="card h-100">
+                <div class="card-body p-3">
+                    <div class="row">
+                        <div class="col-8">
+                            <div class="numbers">
+                                <p class="text-sm mb-0 text-capitalize font-weight-bold">Pengguna Aktif</p>
+                                <h5 class="font-weight-bolder mb-0">
+                                    2,300
+                                    <span class="text-success text-sm font-weight-bolder">+3%</span>
+                                </h5>
+                            </div>
+                        </div>
+                        <div class="col-4 text-end">
+                            <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                                <i style="font-size: 1rem;"
+                                    class="fas fa-lg fa-user-check ps-2 pe-2 text-center text-dark "
+                                    aria-hidden="true"></i>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            </div>
-        </div>
         </div>
         <div class="col-md-3 mb-4">
-        <div class="card">
-            <div class="card-body p-3">
-            <div class="row">
-                <div class="col-8">
-                <div class="numbers">
-                    <p class="text-sm mb-0 text-capitalize font-weight-bold">Pengguna Baharu</p>
-                    <h5 class="font-weight-bolder mb-0">
-                    $103,430
-                    <span class="text-success text-sm font-weight-bolder">+5%</span>
-                    </h5>
+            <div class="card h-100">
+                <div class="card-body p-3">
+                    <div class="row">
+                        <div class="col-8">
+                            <div class="numbers">
+                                <p class="text-sm mb-0 text-capitalize font-weight-bold">Nyahaktif Pengguna</p>
+                                <h5 class="font-weight-bolder mb-0">
+                                    +3,462
+                                    <span class="text-danger text-sm font-weight-bolder">-2%</span>
+                                </h5>
+                            </div>
+                        </div>
+                        <div class="col-4 text-end">
+                            <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                                <i style="font-size: 1rem;"
+                                    class="fas fa-lg fa-user-minus ps-2 pe-2 text-center text-dark "
+                                    aria-hidden="true"></i>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                </div>
-                <div class="col-4 text-end">
-                <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
-                    <i class="ni ni-cart text-lg opacity-10" aria-hidden="true"></i>
-                </div>
-                </div>
-            </div>
             </div>
         </div>
+        <div class="col-md-3 mb-4">
+            <div class="card h-100">
+                <div class="card-body p-3">
+                    <div class="row">
+                        <div class="col-8">
+                            <div class="numbers">
+                                <p class="text-sm mb-0 text-capitalize font-weight-bold">Pengguna Baharu</p>
+                                <h5 class="font-weight-bolder mb-0">
+                                    $103,430
+                                    <span class="text-success text-sm font-weight-bolder">+5%</span>
+                                </h5>
+                            </div>
+                        </div>
+                        <div class="col-4 text-end">
+                            <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                                <i style="font-size: 1rem;"
+                                    class="fas fa-lg fa-user-plus ps-2 pe-2 text-center text-dark "
+                                    aria-hidden="true"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
-    <h6 class="ms-2 mb-3 text-uppercase text-xs font-weight-bolder opacity-6">Paparan Data Pencapaian Perkembangan dan M-CHAT</h6>
+    <h6 class="ms-2 mb-3 text-uppercase text-xs font-weight-bolder opacity-6">Paparan Data Pencapaian Perkembangan dan
+        M-CHAT</h6>
     <div class="row mb-4">
         <!--card kat dashboard 1st line-->
         <div class="col-md-4 mb-4">
-        <div class="card">
-            <div class="card-body p-3">
-            <div class="row">
-                <div class="col-8">
-                <div class="numbers">
-                    <p class="text-sm mb-0 text-capitalize font-weight-bold">Jumlah Kanak-kanak</p>
-                    <h5 class="font-weight-bolder mb-0">
-                    $53,000
-                    <span class="text-success text-sm font-weight-bolder">+55%</span>
-                    </h5>
-                </div>
-                </div>
-                <div class="col-4 text-end">
-                <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
-                    <i class="ni ni-money-coins text-lg opacity-10" aria-hidden="true"></i>
-                </div>
-                </div>
-            </div>
-            </div>
-        </div>
-        </div>
-        <div class="col-md-4 mb-4">
-            <div class="card">
+            <div class="card h-100">
                 <div class="card-body p-3">
-                <div class="row">
-                    <div class="col-8">
-                    <div class="numbers">
-                        <p class="text-sm mb-0 text-capitalize font-weight-bold">Kadar Penilaian Perkembangan Selesai</p>
-                        <h5 class="font-weight-bolder mb-0">
-                        2,300
-                        <span class="text-success text-sm font-weight-bolder">+3%</span>
-                        </h5>
+                    <div class="row">
+                        <div class="col-8">
+                            <div class="numbers">
+                                <p class="text-sm mb-0 text-capitalize font-weight-bold">Jumlah Kanak-kanak</p>
+                                <h5 class="font-weight-bolder mb-0">
+                                    $53,000
+                                    <span class="text-success text-sm font-weight-bolder">+55%</span>
+                                </h5>
+                            </div>
+                        </div>
+                        <div class="col-4 text-end">
+                            <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                                <i style="font-size: 1rem;" class="fas fa-lg fa-child ps-2 pe-2 text-center text-dark "
+                                    aria-hidden="true"></i>
+                            </div>
+                        </div>
                     </div>
-                    </div>
-                    <div class="col-4 text-end">
-                    <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
-                        <i class="ni ni-world text-lg opacity-10" aria-hidden="true"></i>
-                    </div>
-                    </div>
-                </div>
                 </div>
             </div>
         </div>
         <div class="col-md-4 mb-4">
-        <div class="card">
-            <div class="card-body p-3">
-            <div class="row">
-                <div class="col-8">
-                <div class="numbers">
-                    <p class="text-sm mb-0 text-capitalize font-weight-bold">Pengagihan Berisiko Tinggi M-CHAT</p>
-                    <h5 class="font-weight-bolder mb-0">
-                    +3,462
-                    <span class="text-danger text-sm font-weight-bolder">-2%</span>
-                    </h5>
+            <div class="card h-100">
+                <div class="card-body p-3">
+                    <div class="row">
+                        <div class="col-8">
+                            <div class="numbers">
+                                <p class="text-sm mb-0 text-capitalize font-weight-bold">Kadar Penilaian Perkembangan
+                                    Selesai</p>
+                                <h5 class="font-weight-bolder mb-0">
+                                    2,300
+                                    <span class="text-success text-sm font-weight-bolder">+3%</span>
+                                </h5>
+                            </div>
+                        </div>
+                        <div class="col-4 text-end">
+                            <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                            <i style="font-size: 1rem;" class="fas fa-lg fa-list-check ps-2 pe-2 text-center text-dark "
+                            aria-hidden="true"></i>
+                            
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                </div>
-                <div class="col-4 text-end">
-                <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
-                    <i class="ni ni-paper-diploma text-lg opacity-10" aria-hidden="true"></i>
-                </div>
-                </div>
-            </div>
             </div>
         </div>
+        <div class="col-md-4 mb-4">
+            <div class="card h-100">
+                <div class="card-body p-3">
+                    <div class="row">
+                        <div class="col-8">
+                            <div class="numbers">
+                                <p class="text-sm mb-0 text-capitalize font-weight-bold">Pengagihan Berisiko Tinggi
+                                    M-CHAT</p>
+                                <h5 class="font-weight-bolder mb-0">
+                                    +3,462
+                                    <span class="text-danger text-sm font-weight-bolder">-2%</span>
+                                </h5>
+                            </div>
+                        </div>
+                        <div class="col-4 text-end">
+                            <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
+                            <i style="font-size: 1rem;" class="fas fa-lg fa-circle-exclamation ps-2 pe-2 text-center text-dark "
+                            aria-hidden="true"></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 
@@ -176,9 +190,12 @@
                     <thead>
                         <tr>
                             <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">LID.</th>
-                            <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Tajuk Senarai</th>
-                            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Kategori Umur</th>
-                            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Tarikh</th>
+                            <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Tajuk
+                                Senarai</th>
+                            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                Kategori Umur</th>
+                            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                Tarikh</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -227,7 +244,7 @@
             </div>
         </div>
     </div>
-    
+
     <h6 class="ms-2 mb-4 text-uppercase text-xs font-weight-bolder opacity-6">Paparan Data Tip dan intervensi</h6>
     <div class="card mb-3 mb-4">
         <div class="card-body">
@@ -237,8 +254,10 @@
                     <thead>
                         <tr>
                             <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Bil.</th>
-                            <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Tajuk Senarai</th>
-                            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Tarikh</th>
+                            <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Tajuk
+                                Senarai</th>
+                            <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">
+                                Tarikh</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -246,7 +265,7 @@
                         <tr>
                             <td colspan="4" class="text-center mb-0 text-xs">Tiada rekod</td>
                         </tr>-->
-                        
+
                         <tr>
                             <td class="align-middle text-xs">1</td>
                             <td class="align-middle text-xs">Senarai Tips </td>
@@ -257,7 +276,7 @@
                             <td class="align-middle text-xs">Senarai Intervensi</td>
                             <td class="align-middle text-center text-xs">12/02/2023</td>
                         </tr>
-                        
+
                     </tbody>
                 </table>
             </div>
