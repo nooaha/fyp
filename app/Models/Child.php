682c15530ac5546b7688260f433f952b48c096a1
nooahaa <nuhazakiudin@gmail.com>
2024-10-02 17:24:26 +0800

    Add child model

---
+++ app/Models/Child.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Child extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['child_name', 'child_dob', 'child_gender', 'height', 'weight', 'parent_id'];
+
+    protected $casts = [
+        'child_dob' => 'date',
+        'child_gender' => 'string',
+        'height' => 'decimal:2',
+        'weight' => 'decimal:2',
+    ];
+
+    
+    // Define inverse relationship
+    public function parent()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
