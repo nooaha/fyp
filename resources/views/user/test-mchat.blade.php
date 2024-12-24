d014eeb2af3365deee3eeb9bc9007dd0e321daed
nooahaa <nuhazakiudin@gmail.com>
2024-12-25 00:52:48 +0800

    modify layout and guilines for mchat result and test

--- resources/views/user/test-mchat.blade.php
+++ resources/views/user/test-mchat.blade.php
@@ -6,18 +6,27 @@
     <div class="card">
         <div class="card-header pb-0">
             <p>Arahan: Jawab semua soalan. Pilih ‘Ya’ atau ‘Tidak’ pada butang jawapan yang disediakan.</p>
+            <h6 class="mt-0">1. Bagaimana Menganalisa Senarai Semak</h6>
+            <ul>
+                <li>Ibubapa perlu menjawab Ya atau Tidak kepada semua soalan 1 - 23.</li>
+                <li>Enam item yang di gelapkan adalah soalan yang kritikal iaitu 2, 7, 9, 13, 14, 15.</li>
+            </ul>
         </div>
-        <div class="card-body pt-1">
+
+        <div class="card-body pt-0">
             <div class="table-responsive">
                 <table class="table align-items-center mb-0">
                     <thead>
                         <tr>
                             <th class="text-uppercase text-secondary text-xs font-weight-bolder opacity-7">No. Soalan
                             </th>
-                            <th class="text-uppercase text-center text-secondary text-xs font-weight-bolder opacity-7 ps-2">Soalan
-                                Saringan</th>
+                            <th
+                                class="text-uppercase text-center text-secondary text-xs font-weight-bolder opacity-7 ps-2">
+                                Soalan Saringan
+                            </th>
                             <th class="text-center text-uppercase text-secondary text-xs font-weight-bolder opacity-7">
-                                Jawapan</th>
+                                Jawapan
+                            </th>
                         </tr>
                     </thead>
                     <tbody>
@@ -29,22 +38,35 @@
                                         <span>{{ $question->id }}</span>
                                     </td>
                                     <td class="align-middle text-m" style="width: 73%; white-space: normal;">
-                                        <span>{{ $question->question }}</span>
+                                        <span
+                                            style="{{ in_array($question->id, [2, 7, 9, 13, 14, 15]) ? 'font-weight: bold;' : '' }}">
+                                            {{ $question->question }}
+                                        </span>
                                     </td>
-                                    <td class="align-middle text-center text-m ">
-                                        <input type="radio" name="answers[{{ $question->id }}]" value="0" required> Ya
-                                        <input type="radio" name="answers[{{ $question->id }}]" value="1" required> Tidak
-                                    </td>
+                                    <td class="align-middle text-center text-m">
+                                        <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+                                            <!-- Radio button for Yes -->
+                                            <input type="radio" class="btn-check" name="answers[{{ $question->id }}]"
+                                                id="yes_{{ $question->id }}" value="0" autocomplete="off" required>
+                                            <label class="btn btn-outline-success" for="yes_{{ $question->id }}">Ya</label>
 
+                                            <!-- Radio button for No -->
+                                            <input type="radio" class="btn-check" name="answers[{{ $question->id }}]"
+                                                id="no_{{ $question->id }}" value="1" autocomplete="off" required>
+                                            <label class="btn btn-outline-secondary"
+                                                for="no_{{ $question->id }}">Tidak</label>
+                                        </div>
+                                    </td>
                                 </tr>
                             @endforeach
                     </tbody>
                 </table>
-                
+
                 <div class="d-flex justify-content-end">
-                        <a href="{{ route('mchat.index', ['childId' => request('childId')]) }}" class="btn btn-secondary me-2">Batal</a>
-                        <button type="submit" class="btn btn-primary" style="float: right">Hantar</button>
-                    </div>
+                    <a href="{{ route('mchat.index', ['childId' => request('childId')]) }}"
+                        class="btn btn-secondary me-2">Batal</a>
+                    <button type="submit" class="btn btn-primary" style="float: right">Hantar</button>
+                </div>
                 </form>
             </div>
         </div>
--- resources/views/user/test-result.blade.php
+++ resources/views/user/test-result.blade.php
@@ -4,28 +4,69 @@
 <div class="container">
     <h5 class="card-title mb-3">Keputusan Ujian Saringan Modified Checklist Autism Test(M-CHAT)</h5>
 
-    <div class="card mb-3">
+    <div class="card card-frame mb-3">
         <div class="card-body">
             <h6 class="card-title mb-3">Maklum Balas Saringan M-CHAT</h6>
             <div class="row align-items-center">
-                <!-- Peratusan Pencapaian -->
+                <!-- Feedback Section -->
                 <div class="col-md-12">
-                    <p class="mb-0 mb-1">
-                        Skor anak anda adalah <strong> {{ $results->score }}</strong>
+                    <p class="mb-1">
+                        Skor M-CHAT {{ $child->child_name }} adalah <strong>{{ $results->score }}</strong>.
                     </p>
-                    <p class="mb-0 mb-1">
-                        Oleh itu anak anda mungkin mempunyai Risiko Sederhana ASD (Autism Spectrum Disorder)
+                    <p class="mb-1">
+                        Oleh itu anak anda mungkin mempunyai
+                        <span class="badge 
+                        @if ($results->risk_level === 'RISIKO TINGGI')
+                            bg-gradient-danger
+                        @elseif ($results->risk_level === 'RISIKO SEDERHANA')
+                            bg-gradient-warning
+                        @else
+                            bg-gradient-success
+                        @endif">
+                            {{ $results->risk_level }}
+                        </span>  Autism Spectrum Disorder (ASD)
                     </p>
                     <br>
-                    <p class="mb-0 mb-1">
-                        Risiko <strong> {{ $results->risk_level }}</strong> Jumlah skor adalah 3 hingga 7 : Adalah
-                        diterima untuk memintas tindakan susulan dan merujuk segera untuk penilaian diagnostik dan
-                        penilaian kelayakan untuk intervensi awal.
-                    </p>
+                    @if ($results->risk_level === 'RISIKO TINGGI')
+                        <p class="mb-1">
+                            <strong style="color: #D32F2F;">Risiko Tinggi:</strong> Risiko Tinggi menunjukkan jumlah skor
+                            melebihi ambang kritikal.<br>
+                        <ul>
+                            <li>Segera rujuk kepada pakar pediatrik atau pakar psikologi untuk penilaian diagnostik.</li>
+                            <li>Mengatur temujanji untuk penilaian intervensi awal.</li>
+                        </ul>
+                        <strong>Nota:</strong> Penilaian segera dapat membantu dalam menyediakan sokongan dan intervensi
+                        awal.
+                        </p>
+                    @elseif ($results->risk_level === 'RISIKO SEDERHANA')
+                        <p class="mb-1">
+                            <strong style="color: #FFC107;">Risiko Sederhana:</strong> Risiko Sederhana menunjukkan skor
+                            berada antara {{ $moderate_threshold_min }} dan {{ $moderate_threshold_max }}.<br>
+                        <ul>
+                            <li>Lakukan saringan tambahan untuk menentukan keperluan tindakan selanjutnya.</li>
+                            <li>Berbincang dengan pakar pediatrik tentang sebarang kebimbangan atau simptom yang
+                                diperhatikan.</li>
+                        </ul>
+                        <strong>Nota:</strong> Pemantauan berkala adalah penting untuk memastikan perkembangan anak dalam
+                        tahap optimum.
+                        </p>
+                    @else
+                        <p class="mb-1">
+                            <strong style="color: #4CAF50;">Risiko Rendah:</strong> Risiko Rendah menunjukkan jumlah skor
+                            berada di bawah ambang kritikal.<br>
+                        <ul>
+                            <li>Tiada tindakan susulan diperlukan pada masa ini.</li>
+                            <li>Teruskan memantau perkembangan anak secara berkala.</li>
+                        </ul>
+                        <strong>Nota:</strong> Pastikan anak mendapat peluang untuk terus berkembang dengan sokongan
+                        pendidikan dan persekitaran yang positif.
+                        </p>
+                    @endif
                 </div>
             </div>
         </div>
     </div>
+
     <div class="d-flex justify-content-end">
         <a href="{{ route('mchat.index', ['childId' => request('childId')]) }}"
             class="btn btn-secondary me-2">Kembali</a>
